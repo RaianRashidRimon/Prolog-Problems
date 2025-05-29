d8cc684d09c17893766cd039e181e6335d098350
Raian Rashid <rimonraian2@gmail.com>
2025-05-30 00:18:26 +0600

    sum or array values

--- Sum Of Array Values.pl
+++ Sum Of Array Values.pl
@@ -1,7 +1,7 @@
-% Base case: the sum of an empty list is 0
 sum([], 0).
 
-% Recursive case: sum of a list is the head + sum of the tail
+
+
 sum([Head|Tail], Sum) :-
     sum(Tail, TailSum),
     Sum is Head + TailSum.
