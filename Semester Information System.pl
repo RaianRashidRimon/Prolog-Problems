3900d617351e0915b12a8cc9868617f006de45aa
RaianRashidRimon <rimonraian2@gmail.com>
2025-07-03 05:49:55 +0600

    information system

---
+++ Semester Information System.pl
@@ -0,0 +1,113 @@
+% BSc Program Information System
+
+% Facts: course(Semester, CourseCode, CourseTitle, Teacher)
+
+% 2nd Year 1st Semester
+course('2nd Year 1st Semester', 'CSE-201', 'Mathematics-III', 'Dr. Md. Sabbir Alam').
+course('2nd Year 1st Semester', 'CSE-203', 'Computer Ethics And Cyber law', 'Dr. Israt Jahan').
+course('2nd Year 1st Semester', 'CSE-205', 'Numerical Methods', 'Dr. Md. Golam Moazzam').
+course('2nd Year 1st Semester', 'CSE-206', 'Numerical Methods LAB', 'Dr. Md. Golam Moazzam').
+course('2nd Year 1st Semester', 'CSE-207', 'Electronic Devices and Circuits-II', 'Sabrina Sharmin').
+course('2nd Year 1st Semester', 'CSE-208', 'Electronic Devices and Circuits II LAB', 'Sabrina Sharmin').
+course('2nd Year 1st Semester', 'CSE-209', 'Algorithms', 'Mohammad Ashraful Islam').
+course('2nd Year 1st Semester', 'CSE-210', 'Algorithms LAB', 'Mohammad Ashraful Islam').
+course('2nd Year 1st Semester', 'CSE-212', 'Object Oriented Programming LAB (JAVA)', 'Dr. Md. Ezharul Islam').
+
+% 2nd Year 2nd Semester
+course('2nd Year 2nd Semester', 'CSE-251', 'Introduction to Probability and Statistics', 'Md. Habibur Rahman').
+course('2nd Year 2nd Semester', 'CSE-253', 'Digital Logic Design', 'Dr. Jugal Krishna Das').
+course('2nd Year 2nd Semester', 'CSE-254', 'Digital Logic Design LAB', 'Dr. Jugal Krishna Das').
+course('2nd Year 2nd Semester', 'CSE-255', 'Database System', 'Anup Majumder').
+course('2nd Year 2nd Semester', 'CSE-256', 'Database System LAB', 'Anup Majumder').
+course('2nd Year 2nd Semester', 'CSE-257', 'Algorithms-II', 'Mohammad Ashraful Islam').
+course('2nd Year 2nd Semester', 'CSE-258', 'Algorithms-II', 'Mohammad Ashraful Islam').
+course('2nd Year 2nd Semester', 'CSE-259', 'Data and Telecommunication', 'Dr. Md Abul Kalam Azad').
+course('2nd Year 2nd Semester', 'CSE-260', 'Data and Telecommunication LAB', 'Dr. Md Abul Kalam Azad').
+
+% 3rd Year 1st Semester
+course('3rd Year 1st Semester', 'CSE-301', 'Economics', 'Mohammad Adnan Al Nahian').
+course('3rd Year 1st Semester', 'CSE-303', 'Computer Graphics', 'Dr. Mohammad Shorif Uddin').
+course('3rd Year 1st Semester', 'CSE-304', 'Computer Graphics LAB', 'Dr. Mohammad Shorif Uddin').
+course('3rd Year 1st Semester', 'CSE-305', 'Computational Geometry', 'Bulbul Ahammad').
+course('3rd Year 1st Semester', 'CSE-307', 'Computer Architecture and Organisation', 'Dr. Jugal Krishna Das').
+course('3rd Year 1st Semester', 'CSE-309', 'Operating System', 'Dr. Md Abul Kalam Azad').
+course('3rd Year 1st Semester', 'CSE-310', 'Operating System LAB', 'Dr. Md Abul Kalam Azad').
+course('3rd Year 1st Semester', 'CSE-312', 'Web Design And Computer LAB-I', 'Dr. Abu Sayed Md. Mostafizur Rahaman').
+course('3rd Year 1st Semester', 'CSE-314', 'Object Oriented Analysis and Design LAB', 'Sarnali Basak').
+
+% 3rd Year 2nd Semester
+course('3rd Year 2nd Semester', 'CSE-351', 'Management And Accounting', 'Abu Sayef Md Muntaquimul Bari Chowdhuri').
+course('3rd Year 2nd Semester', 'CSE-353', 'Human Computer Interaction', 'Samsun Nahar Khandakar').
+course('3rd Year 2nd Semester', 'CSE-355', 'Introduction To Bioinformatics', 'Bulbul Ahammad').
+course('3rd Year 2nd Semester', 'CSE-357', 'Microprocessor', 'Dr. Israt Jahan').
+course('3rd Year 2nd Semester', 'CSE-358', 'Microprocessor And Assembly Language LAB', 'Dr. Md. Golam Moazzam').
+course('3rd Year 2nd Semester', 'CSE-359', 'Computer Networks', 'Dr. Md. Imdadul Islam').
+course('3rd Year 2nd Semester', 'CSE-360', 'Computer Networks LAB', 'Dr. Md. Imdadul Islam').
+course('3rd Year 2nd Semester', 'CSE-362', 'Web Design And Computer LAB-II', 'Dr. Abu Sayed Md. Mostafizur Rahaman').
+
+% 4th Year 1st Semester
+course('4th Year 1st Semester', 'CSE-401', 'Theory Of Computation And Application', 'Amina Khatun').
+course('4th Year 1st Semester', 'CSE-403', 'Software Engineering And Information System Design', 'Dr. Md. Musfique Anwar').
+course('4th Year 1st Semester', 'CSE-404', 'Software Engineering And ISD LAB', 'Dr. Md. Musfique Anwar').
+course('4th Year 1st Semester', 'CSE-405', 'Digital image processing', 'Dr. Morium Akter').
+course('4th Year 1st Semester', 'CSE-406', 'Digital image processing LAB', 'Dr. Morium Akter').
+course('4th Year 1st Semester', 'CSE-407', 'Wireless network', 'Samsun Nahar Khandakar').
+course('4th Year 1st Semester', 'CSE-410', 'Mobile Application Development LAB', 'Dr. Md. Ezharul Islam').
+course('4th Year 1st Semester', 'CSE-440', 'Research Project / Thesis', 'Dr. Md. Ezharul Islam').
+
+% 4th Year 2nd Semester
+course('4th Year 2nd Semester', 'CSE-451', 'Data Mining And Big Data Analysis', 'Dr. Md. Humayun Kabir').
+course('4th Year 2nd Semester', 'CSE-452', 'Data Mining And Big Data Analysis LAB', 'Dr. Md. Humayun Kabir').
+course('4th Year 2nd Semester', 'CSE-453', 'Artificial Intelligence', 'Dr. Morium Akter').
+course('4th Year 2nd Semester', 'CSE-454', 'Artificial Intelligence LAB', 'Dr. Morium Akter').
+course('4th Year 2nd Semester', 'CSE-455', 'Software Quality Assurance', 'Amina Khatun').
+course('4th Year 2nd Semester', 'CSE-457', 'Machine Learning', 'Dr. Liton Jude Rozario').
+course('4th Year 2nd Semester', 'CSE-458', 'Machine Learning LAB', 'Dr. Liton Jude Rozario').
+course('4th Year 2nd Semester', 'CSE-460', 'IoT LAB', 'Dr. Md. Ezharul Islam').
+course('4th Year 2nd Semester', 'CSE-480', 'Research Project / Thesis', 'Dr. Md. Ezharul Islam').
+
+% Queries
+
+% Get all courses for a semester
+display_courses(Semester) :-
+    course(Semester, Code, Title, Teacher),
+    format('~w - ~w (Assigned Teacher: ~w)~n', [Code, Title, Teacher]), fail.
+display_courses(_).
+
+% Find instructor for a course
+find_instructor(CourseCode) :-
+    course(_, CourseCode, Title, Teacher),
+    format('Course: ~w (~w) is taught by ~w.~n', [CourseCode, Title, Teacher]).
+
+% Find all courses taught by a specific teacher
+find_courses_by_teacher(Teacher) :-
+    course(_, Code, Title, Teacher),
+    format('~w - ~w~n', [Code, Title]), fail.
+find_courses_by_teacher(_).
+
+% Find course code by course title
+find_course_code(Title) :-
+    course(_, Code, Title, _),
+    format('Course code of ~w is ~w~n', [Title, Code]).
+
+% Find course title by course code
+find_course_title(Code) :-
+    course(_, Code, Title, _),
+    format('Course Code: ~w corresponds to Title: ~w~n', [Code, Title]).
+
+% Find course details by course code
+find_course_details(Code) :-
+    course(Semester, Code, Title, Teacher),
+    format('Course Code: ~w (~w) is taught in ~w by ~w.~n', [Code, Title, Semester, Teacher]).
+
+% Find all LAB courses in a specific semester
+find_lab_courses(Semester) :-
+    course(Semester, Code, Title, Teacher),
+    sub_atom(Title, _, _, _, 'LAB'),
+    format('~w - ~w (Assigned Teacher: ~w)~n', [Code, Title, Teacher]), fail.
+find_lab_courses(_).
+
+% Find my supervisor based on the Research Project / Thesis course
+find_my_supervisor :-
+    course(_, 'CSE-480', _, Supervisor),  % Using CSE-480 as it’s the final thesis course
+    format('My research supervisor is ~w.~n', [Supervisor]).
