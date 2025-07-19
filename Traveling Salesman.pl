10318b3af9a8522aca63e48860b54c8e6b349e0c
Raian Rashid <rimonraian2@gmail.com>
2025-07-20 01:04:56 +0600

    traveling salesman

--- Traveling Salesman.pl
+++ Traveling Salesman.pl
@@ -6,25 +6,30 @@ distance(b, c, 35).
 distance(b, d, 25).
 distance(c, d, 30).
 
-% Since the graph is undirected, add reverse distances
 route(X, Y, D) :- distance(X, Y, D).
 route(Y, X, D) :- distance(X, Y, D).
 
-% Find all possible paths and their costs
-tsp(Start, Path, Cost) :-
-    findall(P, permute([b, c, d], P), Permutations),  % Generate all city permutations
-    findall(C, (member(P, Permutations), tour_cost([Start|P], Start, C)), Costs),
-    min_member(Cost, Costs),   % Get minimum cost
-    member(Path, Permutations),
-    tour_cost([Start|Path], Start, Cost).  % Get the best path
 
-% Calculate cost of a tour (visiting all and returning)
+
+
+tsp(Start, Path, Cost) :-
+    findall(P, permute([b, c, d], P), Permutations), 
+    findall(C, (member(P, Permutations), tour_cost([Start|P], Start, C)), Costs),
+    min_member(Cost, Costs), 
+    member(Path, Permutations),
+    tour_cost([Start|Path], Start, Cost).  
+
+
+
+
+
+
 tour_cost([Last], Start, Cost) :- route(Last, Start, Cost).
 tour_cost([A, B | Rest], Start, Cost) :-
     route(A, B, C1),
     tour_cost([B | Rest], Start, C2),
     Cost is C1 + C2.
 
-% Generate permutations of a list (Helper function)
+
 permute([], []).
 permute(L, [H|T]) :- select(H, L, R), permute(R, T).
